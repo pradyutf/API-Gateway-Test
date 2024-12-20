bad853618d0e013148c69de4a939af20ee9ecd22
pradyutf <pradyutfogla@gmail.com>
2024-12-20 11:00:22 +0530

    mq and dockerise

--- service-a/index.js
+++ service-a/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/api/service-a', (req, res) => {
-  res.json({ message: 'Response from Service A' });
+app.get('/service1', (req, res) => {
+  res.send('Response from Service 1');
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Service A is running on port ${PORT}`);
+app.listen(3001, () => {
+  console.log('Service 1 running on port 3001');
 });
+
+
+const amqp = require('amqplib');
+
+async function sendToQueue() {
+    const connection = await amqp.connect('amqp://rabbitmq');
+    const channel = await connection.createChannel();
+    const queue = 'test_queue';
+
+    await channel.assertQueue(queue);
+    channel.sendToQueue(queue, Buffer.from('Message from Service 1'));
+
+    console.log('Message sent to queue');
+}
+
+sendToQueue();
--- service-b/index.js
+++ service-b/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.get('/api/service-b', (req, res) => {
-  res.json({ message: 'Response from Service B' });
+app.get('/service2', (req, res) => {
+  res.send('Response from Service 2');
 });
 
-const PORT = 3002;
-app.listen(PORT, () => {
-  console.log(`Service B is running on port ${PORT}`);
+app.listen(3002, () => {
+  console.log('Service 2 running on port 3002');
 });
+
+
+const amqp = require('amqplib');
+
+async function consumeFromQueue() {
+    const connection = await amqp.connect('amqp://rabbitmq');
+    const channel = await connection.createChannel();
+    const queue = 'test_queue';
+
+    await channel.assertQueue(queue);
+    console.log('Waiting for messages...');
+    channel.consume(queue, (msg) => {
+        console.log('Received:', msg.content.toString());
+        channel.ack(msg);
+    });
+}
+
+consumeFromQueue();
